73af76f5bdc05177c002f5210ed5a5a636c48bce
Kaina <kaina.crow@gmail.com>
2016-05-18 12:47:21 -0700

    fixed all the sizes of png images, added function that allows for randomly positioning sprites along y axis

--- game.js
+++ game.js
@@ -1,21 +1,55 @@
 var gameport = document.getElementById("gameport");
 
-var renderer = PIXI.autoDetectRenderer(400, 400, {backgroundColor: 0x3344ee});
+var renderer = PIXI.autoDetectRenderer(800, 600, {backgroundColor: 0x3344ee});
 gameport.appendChild(renderer.view);
 
 var stage = new PIXI.Container();
 
-var texture = PIXI.Texture.fromImage("submarine.png");
+var subTexture = PIXI.Texture.fromImage("submarine.png");
+var subSprite = new PIXI.Sprite(subTexture); // submarine sprite
 
-var sprite = new PIXI.Sprite(texture);
+subSprite.anchor.x = 0.5;
+subSprite.anchor.y = 0.5;
 
-sprite.anchor.x = 0.5;
-sprite.anchor.y = 0.5;
+subSprite.position.x = 50;
+subSprite.position.y = 200;
 
-sprite.position.x = 200;
-sprite.position.y = 380;
+var sharkTexture = PIXI.Texture.fromImage("shark.png");
+var sharkSprite = new PIXI.Sprite(sharkTexture); // shark sprite
 
-stage.addChild(sprite);
+var orangeFishTexture = PIXI.Texture.fromImage("fish1.png");
+var orangeFishSprite = new PIXI.Sprite(orangeFishTexture); // orangeFish sprite
+
+var blueFishTexture = PIXI.Texture.fromImage("fish2.png");
+var blueFishSprite = new PIXI.Sprite(blueFishTexture);
+
+
+function collision() {
+    if (subSprite.position.x == sharkSprite.position.x){
+        
+    }
+    if (subSprite.position.y == sharkSprite.position.y){
+        
+    }
+}
+
+
+sharkSprite.position.x = 700;
+sharkSprite.position.y = Math.random()*(renderer.height-31)
+
+orangeFishSprite.position.x = 700;
+orangeFishSprite.position.y = Math.random()*(renderer.height-50)
+
+blueFishSprite.position.x = 700;
+blueFishSprite.position.y = Math.random()*(renderer.height-35)
+
+
+
+stage.addChild(subSprite);
+stage.addChild(sharkSprite);
+stage.addChild(orangeFishSprite);
+stage.addChild(blueFishSprite);
+
 var keys = {};
 
 function keyupEventHandler(e) {
@@ -24,27 +58,25 @@ function keyupEventHandler(e) {
 
 function keydownEventHandler(e) {
     keys[e.which] = true;
-    
-    // if(e.keyCode == 87) { // W key pressed
-    //     sprite.position.y -= 1;
-    // }
-    // if(e.keyCode == 83) { // S key pressed
-    //     sprite.position.y += 1;
-    // }
-    // if(e.keyCode == 65) { // A key pressed
-    //     sprite.position.x -= 1;
-    // }
-    // if(e.keyCode == 68) { // D key pressed
-    //     sprite.position.x += 1;
-    // }
 }
 
 document.addEventListener('keydown', keydownEventHandler);
 document.addEventListener('keyup', keyupEventHandler);
 
-var fallingObject = PIXI.Texture.fromImage("shark.png");
-
-
+function movePlayer() {
+    if(keys[87]) { // W key pressed
+        subSprite.position.y -= 5;
+    }
+    if(keys[83]) { // S key pressed
+        subSprite.position.y += 5;
+    }
+    if(keys[65]) { // A key pressed
+        subSprite.position.x -= 5;
+    }
+    if(keys[68]) { // D key pressed
+        subSprite.position.x += 5;
+    }
+}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -53,16 +85,5 @@ function animate() {
 }
 animate();
 setInterval(function () {
-    if(keys[87]) { // W key pressed
-        sprite.position.y -= 5;
-    }
-    if(keys[83]) { // S key pressed
-        sprite.position.y += 5;
-    }
-    if(keys[65]) { // A key pressed
-        sprite.position.x -= 5;
-    }
-    if(keys[68]) { // D key pressed
-        sprite.position.x += 5;
-    }
+    movePlayer();
 }, 1000/60); // 1000ms/60 = refresh 60 times in 1 second
